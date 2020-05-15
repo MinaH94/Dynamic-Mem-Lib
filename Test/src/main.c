5690b548e78170175842fa597ee04aad842991e1
Mina Helmi <59609326+MinaH94@users.noreply.github.com>
2020-05-16 01:43:50 +0200

    main.c: spaces instead of tabs

--- Test/src/main.c
+++ Test/src/main.c
@@ -3,19 +3,18 @@
 #include "test_list.h"
 #include <diag/Trace.h>
 
-
-void main(void)
+void main (void)
 {
-	trace_printf("--- Testing linked list ---\n");
-	TEST_list();
-	trace_printf("---------------------------\n\n");
+   trace_printf ("--- Testing linked list ---\n");
+   TEST_list ();
+   trace_printf ("---------------------------\n\n");
 
-	trace_printf("--- Testing dynamic allocation ---\n");
-	TEST_dynamicAllocation();
-	trace_printf("----------------------------------\n\n");
+   trace_printf ("--- Testing dynamic allocation ---\n");
+   TEST_dynamicAllocation ();
+   trace_printf ("----------------------------------\n\n");
 
-	while (1)
-	{
+   while (1)
+   {
 
-	}
+   }
 }
